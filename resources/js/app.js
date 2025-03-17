b8c9d4772d98a33285d0a071074035241d716dd5
rizkilabs <rizkilabs.dev@gmail.com>
2025-03-18 00:13:45 +0700

    install & config turbolinks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+require("./bootstrap");
+
+//initialize turbolinks
+var Turbolinks = require("turbolinks");
+Turbolinks.start();
+
+//set delay turbolinks
+Turbolinks.setProgressBarDelay(1);
