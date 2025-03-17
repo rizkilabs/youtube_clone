5e48f6080a449ebdb1af1acce230b097ee7b3a5f
rizkilabs <rizkilabs.dev@gmail.com>
2025-03-17 14:25:07 +0700

    add migration & relationship

---
+++ app/Playlist.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Playlist extends Model
+{
+    protected $guarded = [];
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -10,13 +10,18 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'image'
     ];
 
     /**
